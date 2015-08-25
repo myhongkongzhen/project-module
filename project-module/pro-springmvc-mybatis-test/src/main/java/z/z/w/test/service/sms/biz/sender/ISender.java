a25e1f5a1946432d5b4ef0542d05b936bbfdeb74
Z_Z.W <myhongkongzhen@gmail.com>
2015-08-25 15:54:46 +0800

    短信平台下發服務代碼更新

---
+++ project-module/pro-springmvc-mybatis-test/src/main/java/z/z/w/test/service/sms/biz/sender/ISender.java
@@ -0,0 +1,16 @@
+package z.z.w.test.service.sms.biz.sender;
+
+/**************************************************************************
+ * <pre>
+ *     FileName: z.z.w.test.service.sms.biz.sender.ISender.java
+ *         Desc: 
+ *      @author: Z_Z.W - myhongkongzhen@gmail.com
+ *     @version: 2015年8月25日 下午3:36:58 
+ *   LastChange: 2015年8月25日 下午3:36:58 
+ *      History:
+ * </pre>
+ **************************************************************************/
+public interface ISender
+{
+	String sendSms();
+}
