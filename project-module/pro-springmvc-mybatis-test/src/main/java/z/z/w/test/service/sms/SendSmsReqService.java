690f906f71a675b800d54128d96c723c63c3e35f
Z_Z.W <myhongkongzhen@gmail.com>
2015-08-26 09:52:33 +0800

    修改必要代碼

--- project-module/pro-springmvc-mybatis-test/src/main/java/z/z/w/test/service/sms/SendSmsReqService.java
+++ project-module/pro-springmvc-mybatis-test/src/main/java/z/z/w/test/service/sms/SendSmsReqService.java
@@ -2,12 +2,15 @@ package z.z.w.test.service.sms;
 
 import java.util.UUID;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import z.z.w.test.cache.SmsQueueCatch;
+import z.z.w.test.dao.sms.biz.mapper.MerchantChannelBandMapper;
 import z.z.w.test.vo.MerchantSmsSend;
 import z.z.w.test.vo.ParamsVo;
 
@@ -25,7 +28,8 @@ import z.z.w.test.vo.ParamsVo;
 @Scope( value = "prototype" )
 public class SendSmsReqService
 {
-	private static final Logger	logger	= LoggerFactory.getLogger( SendSmsReqService.class );
+	private static final Logger			logger	= LoggerFactory.getLogger( SendSmsReqService.class );
+	private MerchantChannelBandMapper	merchantChannelBandMapper;
 	
 	/**
 	 * Create by : 2015年8月25日 下午12:07:02
@@ -48,8 +52,28 @@ public class SendSmsReqService
 		
 		pv.setMerchantSmsSend( merchantSmsSend );
 		
+		String merchantAccount = "";
+		merchantChannelBandMapper.getChannelCodeByAccount( merchantAccount );
+		
 		logger.debug( "[{}].", pv.toString() );
 		
 		SmsQueueCatch.addData( pv );
+	}
+	
+	/**
+	 * @return the merchantChannelBandMapper
+	 */
+	public MerchantChannelBandMapper getMerchantChannelBandMapper()
+	{
+		return merchantChannelBandMapper;
+	}
+	
+	/**
+	 * @param merchantChannelBandMapper the merchantChannelBandMapper to set
+	 */
+	@Resource
+	public void setMerchantChannelBandMapper( MerchantChannelBandMapper merchantChannelBandMapper )
+	{
+		this.merchantChannelBandMapper = merchantChannelBandMapper;
 	}
 }
