29629c106d0f5f81212e4e7402376e31221a6165
Z_Z.W <myhongkongzhen@gmail.com>
2015-08-26 09:53:27 +0800

    修改必要代碼

---
+++ project-module/pro-springmvc-mybatis-test/src/main/java/z/z/w/test/dao/sms/biz/mapper/MerchantChannelBandMapper.java
@@ -0,0 +1,16 @@
+package z.z.w.test.dao.sms.biz.mapper;
+
+/**************************************************************************
+ * <pre>
+ *     FileName: z.z.w.test.dao.sms.biz.mapper.MerchantChannelBandMapper.java
+ *         Desc: 
+ *      @author: Z_Z.W - myhongkongzhen@gmail.com
+ *     @version: 2015年8月26日 上午9:21:26 
+ *   LastChange: 2015年8月26日 上午9:21:26 
+ *      History:
+ * </pre>
+ **************************************************************************/
+public interface MerchantChannelBandMapper
+{
+	String getChannelCodeByAccount( String merchantAccount );
+}
